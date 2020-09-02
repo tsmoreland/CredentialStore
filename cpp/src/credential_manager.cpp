f87eae3012d77f1ce1ca8222ec9fc2a97e3593ab
Terry Moreland <tsmoreland@live.com>
2020-09-03 00:07:41 +0100

    in progress - implementing credential_manager in C++ - currently doesn't build due to TRAIT classes not fleshed out

---
+++ cpp/src/credential_manager.cpp
@@ -0,0 +1,79 @@
+//
+// Copyright © 2020 Terry Moreland
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), 
+// to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+// and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+// 
+
+#include <credential_manager.h>
+#include "credential_manager_impl_using_traits.h"
+
+namespace win32::credential_store
+{
+
+credential_manager::credential_manager()
+    : m_p_impl(std::make_unique<win32_credential_manager_impl_using_traits>())
+{
+}
+credential_manager::credential_manager(credential_manager &&other) noexcept
+    : m_p_impl(other.m_p_impl.release())
+{
+}
+credential_manager::~credential_manager()
+{
+    m_p_impl.reset();
+}
+
+void credential_manager::add_or_update(credential_t const& credential) const
+{
+    if (!static_cast<bool>(m_p_impl)) {
+    }
+
+    m_p_impl->add_or_update(credential);
+}
+
+credential_manager::optional_credential_t credential_manager::find(wchar_t const* id, credential_type type) const
+{
+    if (!static_cast<bool>(m_p_impl)) {
+    }
+    return m_p_impl->find(id, type);
+}
+
+std::vector<credential_manager::credential_t> credential_manager::find(wchar_t const* filter, bool const search_all) const
+{
+    if (!static_cast<bool>(m_p_impl)) {
+    }
+    //return std::vector<credential_t>();
+    return m_p_impl->find(filter, search_all);
+}
+
+void credential_manager::remove(credential_t const& credential) const
+{
+    if (!static_cast<bool>(m_p_impl)) {
+    }
+    m_p_impl->remove(credential);
+}
+
+credential_manager& credential_manager::operator=(credential_manager&& other) noexcept
+{
+    if (&other == this) {
+        return *this;
+    }
+
+    m_p_impl = std::move(other.m_p_impl);
+
+    return *this;
+}
+
+void swap(credential_manager& first, credential_manager& second) noexcept
+{
+    swap(first.m_p_impl, second.m_p_impl);
+}
+
+}
---
+++ cpp/src/credential_manager_impl.h
@@ -0,0 +1,41 @@
+//
+// Copyright © 2020 Terry Moreland
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), 
+// to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+// and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+// 
+
+#pragma once
+
+#include <optional>
+#include <credential.h>
+
+namespace win32::credential_store
+{
+    class credential_manager_impl 
+    {
+    public:
+        using credential_t = credential<wchar_t>;
+        using optional_credential_t = std::optional<credential_t>;
+
+        credential_manager_impl(const credential_manager_impl& other) = delete;
+        credential_manager_impl(credential_manager_impl&& other) noexcept = delete;
+        virtual ~credential_manager_impl() = default;
+
+        virtual void add_or_update(credential_t const& credential) = 0;
+        [[nodiscard]] virtual optional_credential_t find(wchar_t const* id, credential_type type) const = 0;
+        [[nodiscard]] virtual std::vector<credential_t> find(wchar_t const* filter, bool search_all) const = 0;
+        virtual void remove(credential_t const& credential) const = 0;
+        virtual void remove(wchar_t const* id, credential_type type) const = 0;
+
+        credential_manager_impl& operator=(const credential_manager_impl& other) = delete;
+        credential_manager_impl& operator=(credential_manager_impl&& other) noexcept = default;
+    };
+    
+}
