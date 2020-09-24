0c8defec89314b13c2f8d3b90dcc4e7410f57734
Terry Moreland <tsmoreland@live.com>
2020-09-24 19:24:15 +0100

    updated process_verb to take arguments

--- cpp/src/credential_store_cli/main.cpp
+++ cpp/src/credential_store_cli/main.cpp
@@ -32,43 +32,53 @@ enum class verb_type
     remove,
 };
 
-using verb_processor = void (*)(credential_manager const& manager);
+using verb_processor = void (*)(credential_manager const& manager, std::vector<std::string_view> const& arguments);
 verb_type parse_verb(std::string_view verb_string);
 
-void none(credential_manager const& manager);
-void add(credential_manager const& manager);
-void find(credential_manager const& manager);
-void list(credential_manager const& manager);
-void remove(credential_manager const& manager);
+void none(credential_manager const& manager, std::vector<std::string_view> const& arguments);
+void add(credential_manager const& manager, std::vector<std::string_view> const& arguments);
+void find(credential_manager const& manager, std::vector<std::string_view> const& arguments);
+void list(credential_manager const& manager, std::vector<std::string_view> const& arguments);
+void remove(credential_manager const& manager, std::vector<std::string_view> const& arguments);
 
 int main(int const argc, char* argv[])
 {
-    if (argc < 2)
-    {
-        wcout << L"Usage: credential_store.cli <verb>" << endl;
-        //return 1;
-    }
-
-    std::map<verb_type, verb_processor> verb_to_processor{
-        { verb_type::none, none },
-        { verb_type::add, add },
-        { verb_type::find, find },
-        { verb_type::list, list },
-        { verb_type::remove, remove },
-    };
-
-     auto const* raw_verb = "list";
-
-    //verb_type const verb = parse_verb(argv[1]);
-    verb_type const verb = parse_verb(raw_verb);
-    if (verb == verb_type::none) {
-        wcout << L"Unrecognized verb" << endl;
-        return 2;
-    }
-
-    credential_manager const manager;
     try {
-        verb_to_processor[verb](manager);
+        if (argc < 2)
+        {
+            wcout << L"Usage: credential_store.cli <verb>" << endl;
+            //return 1;
+        }
+
+        std::map<verb_type, verb_processor> verb_to_processor{
+            { verb_type::none, none },
+            { verb_type::add, add },
+            { verb_type::find, find },
+            { verb_type::list, list },
+            { verb_type::remove, remove },
+        };
+
+         auto const* raw_verb = argc >= 2
+             ? argv[1]
+             : "list";
+
+        std::vector<std::string_view> arguments;
+        for (int i=2; i < argc; i++)
+            arguments.emplace_back(argv[i]);
+
+        verb_type const verb = parse_verb(raw_verb);
+        if (verb == verb_type::none) {
+            wcout << L"Unrecognized verb" << endl;
+            return 2;
+        }
+
+        credential_manager const manager;
+        try {
+            verb_to_processor[verb](manager, arguments);
+
+        } catch (std::exception const& ex) {
+            std::cout << "Error: " << ex.what() << endl;
+        }
 
     } catch (std::exception const& ex) {
         std::cout << "Error: " << ex.what() << endl;
@@ -98,23 +108,32 @@ verb_type parse_verb(std::string_view verb_string)
     return verb_type::none;
 }
 
-void none(credential_manager const& manager)
+void none(credential_manager const& manager, std::vector<std::string_view> const& arguments)
 {
+    static_cast<void>(manager);
+    static_cast<void>(arguments);
 }
-void add(credential_manager const& manager)
+void add(credential_manager const& manager, std::vector<std::string_view> const& arguments)
 {
+    static_cast<void>(manager);
+    static_cast<void>(arguments);
 }
-void find(credential_manager const& manager)
+void find(credential_manager const& manager, std::vector<std::string_view> const& arguments)
 {
+    static_cast<void>(manager);
+    static_cast<void>(arguments);
 }
-void list(credential_manager const& manager)
+void list(credential_manager const& manager, std::vector<std::string_view> const& arguments)
 {
+    static_cast<void>(arguments);
     auto credentials =  manager.get_credentials();
 
     for (auto const& credential : credentials) {
         wcout << credential.get_id() << L" " << credential.get_username() << endl;
     }
 }
-void remove(credential_manager const& manager)
+void remove(credential_manager const& manager, std::vector<std::string_view> const& arguments)
 {
+    static_cast<void>(manager);
+    static_cast<void>(arguments);
 }
