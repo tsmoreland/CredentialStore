5eeadbede5a0467e5ea448a2fa7d508a05196e59
Terry Moreland <tsmoreland@live.com>
2020-11-19 23:03:04 +0000

    more units

---
+++ java/credentialstore-service/src/test/java/moreland/win32/credentialstore/CredentialFlagTests.java
@@ -0,0 +1,45 @@
+//
+// Copyright © 2020 Terry Moreland
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), 
+// to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+// and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+package moreland.win32.credentialstore;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class CredentialFlagTests {
+    
+    @ParameterizedTest(name = "{index} => {0}")
+    @MethodSource("credentialFlagValueProvider")
+    void fromInteger_returnsMatchingPreserveType_IfFound(CredentialFlag expected) {
+        var actual = CredentialFlag.fromInteger(expected.getValue());
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void fromInteger_returnsNone_IfNotFound() {
+        var actual = CredentialFlag.fromInteger(42);
+
+        assertEquals(CredentialFlag.NONE, actual);
+    }
+
+
+    private static Stream<CredentialFlag> credentialFlagValueProvider() {
+        return Arrays.stream(CredentialFlag.class.getEnumConstants());
+    }
+
+}
